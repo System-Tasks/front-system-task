0edb4f05b94bca00a6a3ac8022f42dcb2a66a486
Jenn Salazar <jenniferdayanasr@gmail.com>
2025-02-02 17:48:37 -0500

    cambio en ruta principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,15 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace("/login")
+  }, [])
+
   return (
 
     <p className="text-4xl text-center text-gray-700">
